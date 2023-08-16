83c5b57a7a0d7c2e6eeba8e58b5a1b8d8de19eac
gus-aguiar <gustavopma90@gmail.com>
2023-08-16 15:16:37 -0300

    req01

--- pro_filer/actions/beta_actions.py
+++ pro_filer/actions/beta_actions.py
@@ -2,11 +2,15 @@
 # initial commit
 
 
+def get_path_depth(file_path):
+    return file_path.count("/")
+
+
 def show_deepest_file(context):
     if not context["all_files"]:
         print("No files found")
     else:
-        deepest_file = max(context["all_files"], key=len)
+        deepest_file = max(context["all_files"], key=get_path_depth)
         print(f"Deepest file: {deepest_file}")
 
 
