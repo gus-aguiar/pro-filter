c7c6424dfc7a41487a566fec22d18fd093521d5f
gus-aguiar <gustavopma90@gmail.com>
2023-08-21 18:50:59 -0300

    req 4

--- tests/actions/test_show_details.py
+++ tests/actions/test_show_details.py
@@ -1,1 +1,60 @@
+import pytest
+from datetime import datetime
 from pro_filer.actions.main_actions import show_details  # NOQA
+
+
+@pytest.fixture
+def create_temp_file(tmp_path):
+    file_content = "Hello, world!"
+    temp_file_path = tmp_path / "temp_file.txt"
+
+    with open(temp_file_path, "w") as temp_file:
+        temp_file.write(file_content)
+
+    return temp_file_path
+
+
+@pytest.fixture
+def create_temp_directory(tmp_path):
+    temp_dir = tmp_path / "temp_directory"
+    temp_dir.mkdir()
+    return temp_dir
+
+
+def test_show_details_file(create_temp_file, capsys):
+    context = {"base_path": str(create_temp_file)}
+    show_details(context)
+    captured = capsys.readouterr()
+    assert "File name: temp_file.txt" in captured.out
+    assert "File size in bytes:" in captured.out
+    assert "File type: file" in captured.out
+    assert "File extension: .txt" in captured.out
+    assert "Last modified date:" in captured.out
+
+
+def test_show_details_directory(create_temp_directory, capsys):
+    context = {"base_path": str(create_temp_directory)}
+    show_details(context)
+    captured = capsys.readouterr()
+    assert "File name: temp_directory" in captured.out
+    assert "File size in bytes:" in captured.out
+    assert "File type: directory" in captured.out
+    assert "File extension: [no extension]" in captured.out
+    assert "Last modified date:" in captured.out
+
+
+def test_show_details_nonexistent_file(capsys):
+    context = {"base_path": "nonexistent_file.txt"}
+    show_details(context)
+    captured = capsys.readouterr()
+    assert "File 'nonexistent_file.txt' does not exist" in captured.out
+
+
+def test_show_details_date_format(create_temp_file, capsys):
+    context = {"base_path": str(create_temp_file)}
+    show_details(context)
+    captured = capsys.readouterr()
+    date__caputerd = captured.out.splitlines()[-1]
+    assert (
+        date__caputerd == "Last modified date: " + f"{datetime.now().date()}"
+    )
--- tests/actions/test_show_disk_usage.py
+++ tests/actions/test_show_disk_usage.py
@@ -1,1 +1,44 @@
+import pytest
+from unittest.mock import Mock, patch
 from pro_filer.actions.main_actions import show_disk_usage  # NOQA
+
+
+@pytest.fixture
+def create_temp_file_one(tmp_path):
+    file_content = "Hello, world!"
+    temp_file_path = tmp_path / "temp_file_one.txt"
+
+    with open(temp_file_path, "w") as temp_file:
+        temp_file.write(file_content)
+
+    return temp_file_path
+
+
+@pytest.fixture
+def create_temp_file_two(tmp_path):
+    file_content = "Hello, universe!"
+    temp_file_path = tmp_path / "temp_file_two.txt"
+
+    with open(temp_file_path, "w") as temp_file:
+        temp_file.write(file_content)
+
+    return temp_file_path
+
+
+def test_show_disk_usage(create_temp_file_one, create_temp_file_two, capsys):
+    context = {
+        "all_files": [
+            str(create_temp_file_one),
+            str(create_temp_file_two),
+        ]
+    }
+    mock_get_printable_file_path = Mock(
+        return_value="path/do/the/file/file.txt"
+    )
+    with patch(
+        "pro_filer.actions.main_actions._get_printable_file_path",
+        mock_get_printable_file_path,
+    ):
+        show_disk_usage(context)
+    captured = capsys.readouterr()
+    print(captured.out)
